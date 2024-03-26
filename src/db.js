9765e70b4dc565dfef4392e22bb19d47cbe26c5d
Jonathan Fernandez <arcancode@gmail.com>
2024-03-25 22:41:14 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,8 +2,11 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
+    const uri = "mongodb+srv://<username>:<password>@cluster0.qqv0zjy.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
     try {
         await mongoose.connect('mongodb://localhost/store')
+        await mongoose.connect(uri)
         console.log(">>> DB 'Store' connected");
     } catch (error) {
         console.log(error);
